e5345501e18c13ebe4b32ef4f46ba0b536d3b20c
jdkunzeman <jdkunzeman@sbcglobal.net>
2018-10-04 21:40:56 -0500

    Matt working on autonomous setup and hardware type files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rover/DummyRover.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rover/DummyRover.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.Rover;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+
 /**
  * Created by FIRSTUser on 10/22/2017.
  */
@@ -11,6 +14,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "DumRelTele", group = "Relic")
 //@Disabled
 public class DummyRover extends LinearOpMode {
+    OpenGLMatrix lastLocation = null;
+
+    VuforiaLocalizer vuforia;
 
     /* Declare OpMode members. */
     HardwareDummyRover robot = new HardwareDummyRover();     // Use the K9's hardware
@@ -42,8 +48,8 @@ public class DummyRover extends LinearOpMode {
 
 
             // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
-            FLBL = -gamepad1.left_stick_y;
-            FRBR = -gamepad1.right_stick_y;
+            robot.FL.setPower(0.6);
+            robot.BL.setPower(0.6);
 
             robot.FL.setPower(FLBL);
             robot.BL.setPower(FLBL);
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rover/RoverElectricalM.java
@@ -0,0 +1,205 @@
+package org.firstinspires.ftc.teamcode.Rover;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.FRONT;
+
+
+/**
+ * This 2018-2019 OpMode illustrates the basics of using the Vuforia localizer to determine
+ * positioning and orientation of robot on the FTC field.
+ * The code is structured as a LinearOpMode
+ *
+ * Vuforia uses the phone's camera to inspect it's surroundings, and attempt to locate target images.
+ *
+ * When images are located, Vuforia is able to determine the position and orientation of the
+ * image relative to the camera.  This sample code than combines that information with a
+ * knowledge of where the target images are on the field, to determine the location of the camera.
+ *
+ * This example assumes a "square" field configuration where the red and blue alliance stations
+ * are on opposite walls of each other.
+ *
+ * From the Audience perspective, the Red Alliance station is on the right and the
+ * Blue Alliance Station is on the left.
+
+ * The four vision targets are located in the center of each of the perimeter walls with
+ * the images facing inwards towards the robots:
+ *     - BlueRover is the Mars Rover image target on the wall closest to the blue alliance
+ *     - RedFootprint is the Lunar Footprint target on the wall closest to the red alliance
+ *     - FrontCraters is the Lunar Craters image target on the wall closest to the audience
+ *     - BackSpace is the Deep Space image target on the wall farthest from the audience
+ *
+ * A final calculation then uses the location of the camera on the robot to determine the
+ * robot's location and orientation on the field.
+ *
+ * @see VuforiaLocalizer
+ * @see VuforiaTrackableDefaultListener
+ * see  ftc_app/doc/tutorial/FTC_FieldCoordinateSystemDefinition.pdf
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
+ *
+ * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
+ * is explained below.
+ */
+
+public class RoverElectricalM {
+
+    /*
+     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+     * web site at https://developer.vuforia.com/license-manager.
+     *
+     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+     * random data. As an example, here is a example of a fragment of a valid key:
+     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+     * Once you've obtained a license key, copy the string from the Vuforia web site
+     * and paste it in to your code on the next line, between the double quotes.
+     */
+    private static final String VUFORIA_KEY = "AXLb9ZD/////AAAAGc+ylHTIf0+aorS8rw6aoBRMAiybD7XCkifjVKb1gFrWJ+pZOL6huLnful+ArD+R2XN3/ZGcwQl6+4jsRj2e3Y82Sm/yTgANmCQEqhIqLjfWNePdOqmT0apncNRVE8YfklK+VRNs976s0xR2rEPIl4tNaYoGOHqaJl8JfIrZ5CjIIxKV55C5PUdzzgAxR3NS8hR7wGu5H0rX1of4shVf1Nncn3WNKTrsOU//PPBjgE79RIN3G5aUC54lMNkzMfaJ2FwAfTXoMbSUygQiGu1Sh0UizQpgjqzPH8gIt6v8qt542i4Pk5T+gbrculkfzvFhzMQu81EyP2v4TfCNmCsSrFQdRl2Z7pTbWddtn5//e6Px";
+
+    // Since ImageTarget trackables use mm to specifiy their dimensions, we must use mm for all the physical dimension.
+    // We will define some constants and conversions here
+    private static final float mmPerInch        = 25.4f;
+    private static final float mmFTCFieldWidth  = (12*6) * mmPerInch;       // the width of the FTC field (from the center point to the outer panels)
+    private static final float mmTargetHeight   = (6) * mmPerInch;          // the height of the center of the target image above the floor
+
+    // Select which camera you want use.  The FRONT camera is the one on the same side as the screen.
+    // Valid choices are:  BACK or FRONT
+    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = FRONT;
+
+    private OpenGLMatrix lastLocation = null;
+    private boolean targetVisible = false;
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    VuforiaLocalizer vuforia;
+
+    public boolean bLedOn = true;
+
+    public DcMotor FLMotor = null;
+    public DcMotor BLMotor = null;
+    public DcMotor FRMotor = null;
+    public DcMotor BRMotor = null;
+
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    public RoverElectricalM() {
+    }
+
+    public void init(HardwareMap ahwMap) {
+
+        hwMap = ahwMap;
+
+        BLMotor = hwMap.dcMotor.get("BL");
+        BRMotor = hwMap.dcMotor.get("BR");
+        FLMotor = hwMap.dcMotor.get("FL");
+        FRMotor = hwMap.dcMotor.get("FR");
+
+        FLMotor.setDirection(DcMotor.Direction.REVERSE);
+        BLMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        BLMotor.setPower(0);
+        BRMotor.setPower(0);
+        FLMotor.setPower(0);
+        FRMotor.setPower(0);
+
+        BLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER); //was RUN_WITHOUT_ENCODERS
+        BRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public static void moveMotor(DcMotor motor, double pwr){
+        if(pwr >= 1.0){
+            motor.setPower(1.0);
+        }
+        else if(pwr <= -1.0){
+            motor.setPower(-1.0);
+        }
+        else {
+            motor.setPower(pwr);
+        }
+    }
+
+    public static void moveServo(Servo servo, double pos){
+        if(pos >= 1.0){
+            servo.setPosition(1.0);
+        }
+        else if (pos <= 0.0){
+            servo.setPosition(0.0);
+        }
+        else {
+            servo.setPosition(pos);
+        }
+    }
+
+    public void moveLeftSide(double lPow){
+        lPow = (lPow >= 1.0) ? 1.0 : lPow;
+        lPow = (lPow <= -1.0) ? -1.0 : lPow;
+        BLMotor.setPower(lPow);
+        FLMotor.setPower(lPow);
+    }
+
+    public void moveRightSide(double rPow){
+        rPow = (rPow >= 1.0) ? 1.0 : rPow;
+        BRMotor.setPower(rPow);
+        FRMotor.setPower(rPow);
+    }
+
+    public void moveDrivetrain(double lPow, double rPow){
+        moveLeftSide(lPow);
+        moveRightSide(rPow);
+    }
+
+    public void stopDrivetrain(){
+        moveDrivetrain(0,0);
+    }
+
+
+    public void waitForTick(long periodMs) throws InterruptedException {
+
+        long remaining = periodMs - (long) period.milliseconds();
+
+        if (remaining > 0)
+            Thread.sleep(remaining);
+
+        period.reset();
+
+    }
+    public double power(double val){
+        int sgn = (val >= 0) ? 1 : -1;
+        double holder = Math.pow(val, 2.0) * sgn;
+        return holder;
+    }
+}
+
+
